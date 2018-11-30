30f7bf08ee04d06f6fc694f518e05312ada0f69b
Oliver Bell <freshollie@gmail.com>
2018-11-30 14:22:55 +0000

    Run acceptence tests on deployment

---
+++ jest.acceptence.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "roots": [
+    "<rootDir>"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": ".*.spec.ts?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  "resetMocks": true,
+  "verbose": true
+}
